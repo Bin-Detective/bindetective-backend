0805e7ed7c2e9b2f7984859e497119edfcc76ffb
Mega Nugraha <monmega110@gmail.com>
2024-11-25 03:44:23 +0700

    Add authentication middleware and list collections route to prediction router

--- src/routers/predictRoutes.js
+++ src/routers/predictRoutes.js
@@ -8,7 +8,12 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const router = express.Router(); // Create router object to define root paths
 
+// Apply the middleware to all routes
+router.use(authenticateToken);
+
 // Define routes and associate each route with its respective handlers.
 router.post("/", upload.single("image"), handlers.handleImagePredict);
+
+router.get("/collections", handlers.handleListCollection);
 
 module.exports = router;
